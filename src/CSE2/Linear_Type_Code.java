984ae9151f04060431c0e03be0b4064ee0ae3512
dakshanyakancharla12 <dakshanyakancharla08@gmail.com>
2026-09-07 14:09:40 +0530

    Add files via upload

---
+++ src/CSE2/Linear_Type_Code.java
@@ -0,0 +1,11 @@
+package CSE2;
+
+public class Linear_Type_Code {
+	public static void main(String[] args) {
+		int n = 3;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/CSE2/time_constant_Code.java
@@ -0,0 +1,10 @@
+package CSE2;
+
+public class time_constant_Code {
+	public static void main(String[] args) {
+		int[] arr = {10, 20, 30, 40, 50};
+		
+		int value = arr[2];
+		
+		System.out.println("Value = " + value);	}
+}
